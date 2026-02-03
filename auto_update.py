834cdf5b6982ff1de34b32d7133d000fa2d09579
Dominik Weckmüller <dominik@geo.rocks>
2026-02-03 15:50:07 +0100

    add copernicus services products

--- auto_update.py
+++ auto_update.py
@@ -15,6 +15,17 @@ links = [
     "https://github.com/giswqs/Planetary-Computer-Catalog/raw/master/pc_catalog.json",
     "https://github.com/giswqs/NASA-CMR-STAC/raw/master/nasa_cmr_catalog.tsv",
     "https://github.com/giswqs/NASA-CMR-STAC/raw/master/nasa_cmr_catalog.json"
+    # Copernicus Metadata
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/tsv/copernicus_atmosphere.tsv",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/json/copernicus_atmosphere.json",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/tsv/copernicus_climate.tsv",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/json/copernicus_climate.json",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/tsv/copernicus_emergency.tsv",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/json/copernicus_emergency.json",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/tsv/copernicus_land.tsv",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/json/copernicus_land.json",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/tsv/copernicus_marine.tsv",
+    "https://github.com/do-me/Copernicus-Services-Products-Metadata/raw/main/outputs/json/copernicus_marine.json"
 ]
 
 for link in links:
